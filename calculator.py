bc956b1e847b9c52c2452ed00e4b506302e713aa
mddaaa <mirkin310@gmail.com>
2026-04-18 13:28:11 +0700

    добавил функцию сложения

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print("1. Сложение")
+    choice = input("Выбери: ")
+    if choice == "1":
+        x = float(input("Число 1: "))
+        y = float(input("Число 2: "))
+        print(f"Ответ: {add(x, y)}")
+
