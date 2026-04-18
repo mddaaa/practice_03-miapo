9bffbd17109bbcaf8146c1aa08e333a52ce1fc43
mddaaa <mirkin310@gmail.com>
2026-04-18 13:32:07 +0700

    добавил функцию вычитания

--- calculator.py
+++ calculator.py
@@ -1,12 +1,10 @@
-def add(a, b):
-    return a + b
-
+def subtract(a, b):
+    return a - b
 
 if __name__ == "__main__":
-    print("1. Сложение")
+    print("1. Вычитание")
     choice = input("Выбери: ")
     if choice == "1":
         x = float(input("Число 1: "))
         y = float(input("Число 2: "))
-        print(f"Ответ: {add(x, y)}")
-
+        print(f"Ответ: {subtract(x, y)}")
