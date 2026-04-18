9dccc047366a37a1330ffd42c1c76bf6174bbcf4
mddaaa <mirkin310@gmail.com>
2026-04-18 13:35:34 +0700

    добавил функцию деления

--- calculator.py
+++ calculator.py
@@ -1,10 +1,12 @@
-def multiply(a, b):
-    return a * b
+def divide(a, b):
+    if b == 0:
+        return "На ноль делить нельзя"
+    return a / b
 
 if __name__ == "__main__":
-    print("1. Умножение")
+    print("1. Деление")
     choice = input("Выбери: ")
     if choice == "1":
         x = float(input("Число 1: "))
         y = float(input("Число 2: "))
-        print(f"Ответ: {multiply(x, y)}")
+        print(f"Ответ: {divide(x, y)}")
