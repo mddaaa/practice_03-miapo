5e15c6db16d6e27cfd60334a7c74ae11b7d3595a
mddaaa <mirkin310@gmail.com>
2026-04-18 13:34:40 +0700

    добавил функцию умножения

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
-def subtract(a, b):
-    return a - b
+def multiply(a, b):
+    return a * b
 
 if __name__ == "__main__":
-    print("1. Вычитание")
+    print("1. Умножение")
     choice = input("Выбери: ")
     if choice == "1":
         x = float(input("Число 1: "))
         y = float(input("Число 2: "))
-        print(f"Ответ: {subtract(x, y)}")
+        print(f"Ответ: {multiply(x, y)}")
